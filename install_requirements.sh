98d19537ddb04a0594da3e700ab73ce06521275c
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-03-20 19:35:06 -0500

    Update requirements - remove pycortex and lapy

--- install_requirements.sh
+++ install_requirements.sh
@@ -21,13 +21,13 @@ source "$venv_name/bin/activate"
 # Install required packages
 echo "Installing required packages in virtual environment $venv_name..."
 pip install pydantic rich pybids nibabel pydicom nilearn matplotlib pandas
-pip install lapy antspyx antspynet
+pip install antspyx antspynet
 
 pip install ipython ipykernel ipywidgets
 
 
 # Install the latest release of pycortex from pip
 # pip install setuptools wheel numpy cython
-# pip install pycortex
+# pip install pycortex lapy
 
 pip freeze > requirements.txt
