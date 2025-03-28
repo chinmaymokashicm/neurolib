f88a3545c9bf9ab01bcd37cf0d87c3f854436d63
Mokashi,Chinmay <CMokashi@mdanderson.org>
2025-03-28 17:46:12 -0500

    Fix asttokens version to avoid conflicts with devtools

--- install_requirements.sh
+++ install_requirements.sh
@@ -21,13 +21,15 @@ source "$venv_name/bin/activate"
 # Install required packages
 echo "Installing required packages in virtual environment $venv_name..."
 pip install pydantic rich pybids nibabel pydicom nilearn matplotlib pandas numpy
-pip install antspyx antspynet scikit-image trimesh pyvista
-pip install trame[all] # Install for pyvista visualization
+pip install antspyx antspynet scikit-image
 
 pip install ipython ipykernel ipywidgets
 
 # Install the latest release of pycortex from pip
 # pip install setuptools wheel numpy cython
 # pip install pycortex lapy
+
+# Change version of asttokens to >=2.0.0,<3.0.0
+pip install asttokens==2.1.0
 
 pip freeze > requirements.txt
